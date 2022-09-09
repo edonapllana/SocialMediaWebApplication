a36357fa8924b825bf0f1270b40e1828d9539026
edonapllana <edona.pllana0@gmail.com>
2022-09-10 00:24:57 +0200

    initializing our express app and setting up body parser to send our requests properly

--- server/index.js
+++ server/index.js
@@ -2,3 +2,9 @@ import bodyParser from 'body-parser';
 import cors from 'cors';
 import express from 'express';
 import mongoose from 'mongoose';
+
+const app = express();
+
+app.use(bodyParser.json({limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }));
+app.use(cors());
