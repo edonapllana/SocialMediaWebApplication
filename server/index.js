f20223aa2a81229a02005299462868f501588fab
edonapllana <edona.pllana0@gmail.com>
2022-09-11 01:19:56 +0200

    connecting the app with mongodb

--- server/index.js
+++ server/index.js
@@ -8,3 +8,17 @@ const app = express();
 app.use(bodyParser.json({limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }));
 app.use(cors());
+
+const CONNECTION_URL =
+	'mongodb+srv://testUser:test123123@cluster0.anhqq.mongodb.net/?retryWrites=true&w=majority';
+const PORT = process.env.PORT || 5000;
+mongoose
+	.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTipology: true })
+	.then(() =>
+		app.listen(PORT, () => console.log(`Server running on Port: ${PORT}`)))
+	.catch((error) => console.log(error.message));
+
+mongoose.set('useFindAndModify', false);
+
+
+
