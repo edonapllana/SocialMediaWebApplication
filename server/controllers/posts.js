3a8715c934b9a96cfeb5000bad22b1570f66e08a
edonapllana <edona.pllana0@gmail.com>
2022-09-11 02:12:42 +0200

    Create Posts logic in server side

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -6,12 +6,25 @@ const getPosts = async(req, res) => {
 
 		res.status(200).json(postMessage);
 	} catch (error) {
-		res.status(404).json({message: error.message});
+		res.status(404).json({
+			message: error.message
+		});
 	}
 }
 
-const createPosts = (req, res) => {
+const createPosts = async (req, res) => {
+	const post = req.body;
 
+	const newPost = PostMessage.new(post);
+	try {
+		await newPost.save();
+
+		res.status(201).json(newPost);
+	} catch (error) {
+		res.status(409).json({
+			message: error.message
+		})
+	}
 }
 
 module.exports = {
